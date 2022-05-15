d8874ade0b99d1ffbab36f25bbae2a5d799addc5
valentin-ruggieri <valeru.251@gmail.com>
2022-05-15 02:15:03 -0300

    Desafio 05 parte 2

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8000;
-const path = require('path');
-const productosRouter = require('./routers.js');
+const path = require("path");
+const productosRouter = require("./routers.js");
 
-
-const server = app.listen(PORT, ()=>{
+const server = app.listen(PORT, () => {
   console.log(`Servidor listo en el puerto ${PORT} ✅`);
 });
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('views', path.join(__dirname, 'views' ));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use('/', productosRouter);
+app.use("/", productosRouter);
 
-
-server.on('error', (error) => console.log('Hubo un error ' + error));
+server.on("error", (error) => console.log("Hubo un error " + error));
--- routers.js
+++ routers.js
@@ -1,17 +1,35 @@
-const express= require('express');
+const express = require("express");
 const router = express.Router();
-const productos = require('./productos');
-const imgRandom = require('./imgRandom');
+const productos = require("./productos");
+const imgRandom = require("./imgRandom");
 
+let web = [];
 
-router.get('/', (req, res)=>{
-  res.render('plantilla.ejs', {productos, imgRandom: imgRandom()});
+router.post("/form", (req, res) => {
+  const theme = req.body.theme;
+  web.splice(0, 1, theme);
+  res.redirect("/form");
 });
 
-router.post('/productos', (req, res)=>{
+router.get("/productos", (req, res) => {
+  res.render("productos.ejs", {
+    productos,
+    imgRandom: imgRandom(),
+    theme: web[0],
+  });
+});
+
+router.get("/form", (req, res) => {
+  res.render("form.ejs", { productos, imgRandom: imgRandom(), theme: web[0] });
+});
+router.get("/", (req, res) => {
+  res.render("home.ejs", { productos, imgRandom: imgRandom(), theme: web[0] });
+});
+
+router.post("/productos", (req, res) => {
   const producto = req.body;
   productos.push(producto);
-  res.redirect('/');
+  res.redirect("/productos");
 });
 
 module.exports = router;
